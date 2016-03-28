811bfcf1bed1a463a49cf39c5e8d450e1c7d5d56
mediocretes <nathan@expectedbehavior.com>
2016-03-28 16:54:11 -0400

    change name of instrumental project key to indicate which key is required

--- exampleConfig.js
+++ exampleConfig.js
@@ -7,7 +7,7 @@ See the offical Statsd exampleConfig.js for more StatsD options.
 , backends: ["statsd-instrumental-backend" ]
 , debug: false
 , instrumental: {
-    key: "INSTRUMENTAL_API_KEY", // REQUIRED
+    key: "PROJECT_API_TOKEN", // REQUIRED
     secure: true, // OPTIONAL (boolean), whether or not to use secure protocol to connect to Instrumental, default true
     verify_cert: true, // OPTIONAL (boolean), should we attempt to verify the server certificate before allowing communication, default true
     timeout: 10000, // OPTIONAL (integer), number of milliseconds to wait for establishing a connection to Instrumental before giving up, default 10s
