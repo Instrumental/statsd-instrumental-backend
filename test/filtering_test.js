c27b1c8f32930f3eee42f5814663ab5ace7e19a7
Nathan Acuff <nathan@expectedbehavior.com>
2017-03-22 17:33:15 -0400

    many formatting and javascript comprehension changes to tests, no (intentional) functionality changes.  mostly adding ; and removing superfluous vars

--- test/filtering_test.js
+++ test/filtering_test.js
@@ -5,7 +5,7 @@ var dummy_events =  { on: function(e){ } };
 var now = Math.round(new Date().getTime() / 1000);
 
 test('no filtering is done if no config specified', function (t) {
-  var metrics = {
+  metrics = {
     counters: { 'my.test.1': 2805 },
     counter_rates: { 'my.test.1.rate': 280.5 },
     gauges: { 'my.gauge.1': 100 },
@@ -14,33 +14,33 @@ test('no filtering is done if no config specified', function (t) {
 
   instrumental.init(now, config, dummy_events);
 
-  var payload = instrumental.build_payload(metrics);
-  t.assert(payload.length === 4)
+  payload = instrumental.build_payload(metrics);
+  t.assert(payload.length === 4);
 
-  t.pass()
+  t.pass();
   t.end();
 });
 
 test('no filtering is done if emptyconfig specified', function (t) {
-  var metrics = {
+  metrics = {
     counters: { 'my.test.1': 2805 },
     counter_rates: { 'my.test.1.rate': 280.5 },
     gauges: { 'my.gauge.1': 100 },
     sets: { 'my.set.1': ['1','2','3'] }
   };
 
+  config.instrumental.metricFilters = [];
   instrumental.init(now, config, dummy_events);
 
-  config.instrumental.metricFilters = [];
-  var payload = instrumental.build_payload(metrics);
-  t.assert(payload.length === 4)
+  payload = instrumental.build_payload(metrics);
+  t.assert(payload.length === 4);
 
-  t.pass()
+  t.pass();
   t.end();
 });
 
 test('regex filters work as expected', function (t) {
-  var metrics = {
+  metrics = {
     counters: { 'filter.start.expression': 2805,
                 'filter.middle.expression': 2805 },
     counter_rates: { 'filter.end.expression': 280.5 },
@@ -51,10 +51,9 @@ test('regex filters work as expected', function (t) {
   config.instrumental.metricFilters = [/^filter.start.*/, /.*filter.end$/, /\.middle\./];
   instrumental.init(now, config, dummy_events);
 
-  var payload = instrumental.build_payload(metrics);
-  t.assert(payload.length === 2)
+  payload = instrumental.build_payload(metrics);
+  t.assert(payload.length === 2);
 
-  config.instrumental.metricFilters = [];
-  t.pass()
+  t.pass();
   t.end();
 });
--- test/instrumental_test.js
+++ test/instrumental_test.js
@@ -3,18 +3,18 @@ var instrumental = require("../lib/instrumental.js");
 var config = require("../exampleConfig.js").config;
 
 test('counter_rate should not report if disabled in configuration', function (t) {
-  var metrics = {
+  metrics = {
     counters: { 'my.test.1': 2805 },
     counter_rates: { 'my.test.1': 280.5 }
   };
 
-  var now = Math.round(new Date().getTime() / 1000);
-  var dummy_events =  { on: function(e){ } };
+  now = Math.round(new Date().getTime() / 1000);
+  dummy_events =  { on: function(e){ } };
   config.instrumental.recordCounterRates = true;
-  instrumental.init(now, config, dummy_events)
+  instrumental.init(now, config, dummy_events);
 
   // Enable rate counters and ensure they are recorded
-  var payload = instrumental.build_payload(metrics)
+  payload = instrumental.build_payload(metrics);
   
   // TODO: What's with the fucking space on the end of this string?
    t.assert(payload.indexOf("gauge_absolute my.test.1.rate 280.5 ") > -1, "Expected a rate metric, got: " + JSON.stringify(payload))
@@ -23,7 +23,7 @@ test('counter_rate should not report if disabled in configuration', function (t)
   config.instrumental.recordCounterRates = false;
   instrumental.init(now, config, dummy_events);
 
-  var payload = instrumental.build_payload(metrics);
+  payload = instrumental.build_payload(metrics);
   payload.forEach(function(instrumental_metric) {
     if (instrumental_metric.indexOf("rate") > -1) {
       t.fail("Should not be any rate metrics: " + instrumental_metric);
@@ -36,37 +36,37 @@ test('counter_rate should not report if disabled in configuration', function (t)
 });
 
 test('metricPrefix is used if present in configuration', function (t) {
-  var metrics = {
+  metrics = {
     counters: { 'my.test.1': 2805 }
   };
 
-  var now = Math.round(new Date().getTime() / 1000);
-  var dummy_events =  { on: function(e){ } };
+  now = Math.round(new Date().getTime() / 1000);
+  dummy_events =  { on: function(e){ } };
 
   config.instrumental.metricPrefix = "testprefix";
-  instrumental.init(now, config, dummy_events)
+  instrumental.init(now, config, dummy_events);
 
-  var payload = instrumental.build_payload(metrics);
+  payload = instrumental.build_payload(metrics);
 
-   t.assert(payload.indexOf("increment testprefix.my.test.1 2805 ") > -1, "Metric name was not prefixed properly (got " + JSON.stringify(payload) + ")")
+  t.assert(payload.indexOf("increment testprefix.my.test.1 2805 ") > -1, "Metric name was not prefixed properly (got " + JSON.stringify(payload) + ")");
 
   t.end();
 });
 
 test('metricPrefix ending with dots wont send double dots', function (t) {
-  var metrics = {
+  metrics = {
     counters: { 'my.test.1': 2805 }
   };
 
-  var now = Math.round(new Date().getTime() / 1000);
-  var dummy_events =  { on: function(e){ } };
+  now = Math.round(new Date().getTime() / 1000);
+  dummy_events =  { on: function(e){ } };
 
   config.instrumental.metricPrefix = "testprefix.";
-  instrumental.init(now, config, dummy_events)
+  instrumental.init(now, config, dummy_events);
 
-  var payload = instrumental.build_payload(metrics);
+  payload = instrumental.build_payload(metrics);
 
-   t.assert(payload.indexOf("increment testprefix.my.test.1 2805 ") > -1, "Metric name was not prefixed properly (got " + JSON.stringify(payload) + ")")
+  t.assert(payload.indexOf("increment testprefix.my.test.1 2805 ") > -1, "Metric name was not prefixed properly (got " + JSON.stringify(payload) + ")");
 
   t.end();
 });
