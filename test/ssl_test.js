ff40cf0cd4eae6b34c66d71cc96f638d853404d0
Jason Gladish <jason@expectedbehavior.com>
2020-04-14 17:52:59 -0400

    Fix tests to account for recently rotated cert
    These integration tests previously relied on the old cert configuration
    in production/smoke. Since rotating the cert they failed.
    
    We add a fixture intermediate cert to separate the test environment from
    the production bundled certs or order to test specifying a particular
    cert bundle.
    
    Since this package now relies solely on system certs we comment out the
    related cert selection tests. If we add certs to the bundle later we
    will uncomment these in order to adequately test cert selection.
    
    These are just test changes so don't require a release.
    
    Co-authored-by: Nathan Acuff <nathan@expectedbehavior.com>

--- test/ssl_test.js
+++ test/ssl_test.js
@@ -15,12 +15,7 @@ test('specifying a valid and working cert bundle works', function(t) {
   oldTime = Math.round(new Date().getTime() / 1000);
 
   var certs = [];
-  var certDir = path.join(__dirname, "..", "certs/");
-  fs.readdirSync(certDir).forEach(function(filename) {
-    if (filename.match(/ca\.pem$/) && !filename.match(/20/)) {
-      certs.push(certDir + filename);
-    }
-  });
+  certs.push(path.join(__dirname, "..", "test", "fixtures", "current_production_intermediate_cert.pem"));
 
   TestHelper.config.instrumental.host = "smoke-collector.instrumentalapp.com";
   TestHelper.config.instrumental.caCertFiles = certs;
@@ -114,213 +109,217 @@ test('node default certs are used by default', function(t) {
   });
 });
 
-test('before cert expiration, fallback to newest cert bundle and stick if node default certs fail', function(t) {
-  setup(t);
+// Skipping this test for now since we rely on system certs. If we add new cert
+// bundles then we should add this back in.
+// test('before cert expiration, fallback to newest cert bundle and stick if node default certs fail', function(t) {
+//   setup(t);
+//
+//   realCurrentTime = Math.round(new Date().getTime() / 1000);
+//
+//   var time = new Date(Date.parse("2018-01-01"));
+//   timekeeper.travel(time); // Travel to that date.
+//
+//   var metricName = "test.metric"+Math.random();
+//
+//   TestHelper.config.instrumental.tlsVariationTimeout = 500;
+//
+//   var timeBetweenSends = 2000;
+//   var actions = [];
+//
+//   // node default certs
+//   actions.push(function(){
+//     TestHelper.config.instrumental.host = "smoke-collector.instrumentalapp.com";
+//     sendMetric(metricName, realCurrentTime); // using node default certs
+//   });
+//   actions.push(function(){
+//     sendMetric(metricName, realCurrentTime, {skipInit: true}); // using node default certs
+//   });
+//
+//   // New cert bundle
+//   actions.push(function(){
+//     TestHelper.config.instrumental.port = 8000;
+//     sendMetric(metricName, realCurrentTime); // node default certs fail
+//   });
+//   actions.push(function(){
+//     TestHelper.config.instrumental.port = 8001;
+//     sendMetric(metricName, realCurrentTime); // bundle
+//   });
+//   actions.push(function(){
+//     sendMetric(metricName, realCurrentTime, {skipInit: true}); // bundle
+//   });
+//
+//   // Back to node default
+//   actions.push(function(){
+//     TestHelper.config.instrumental.port = 8000;
+//     sendMetric(metricName, realCurrentTime); // bundle fails
+//   });
+//   actions.push(function(){
+//     TestHelper.config.instrumental.port = 8001;
+//     sendMetric(metricName, realCurrentTime); // node default
+//   });
+//   actions.push(function(){
+//     sendMetric(metricName, realCurrentTime, {skipInit: true}); // node default
+//   });
+//
+//   actions.forEach(function(action, index){
+//     setTimeout(action, index*timeBetweenSends);
+//   });
+//
+//   checkForMetric(metricName, {
+//     expectedSum: 6, // failures don't get sent
+//     found: function(){
+//       var cert_log_messages =
+//         TestHelper.log.filter(function(entry){return entry.match(/\bcert/i)});
+//       var expected_messages = [
+//         "Adding node default ssl cert option",
+//         'Found valid cert bundle: equifax.2018-08-19',
+//         'Found valid cert bundle: geotrust.2018-08-19',
+//         'Found valid cert bundle: rapidssl.2018-08-19',
+//         'Found valid cert bundle: digicert_intermediate',
+//         'Found valid cert bundle: digicert_root',
+//         'Found valid cert bundle: rapidssl',
+//         "Attempting new cert config: node default",
+//         "Using certs: node default", // 1
+//         "Using certs: node default", // 2
+//         "Using certs: node default", // fail
+//         "Attempting new cert config: bundles equifax.2018-08-19 geotrust.2018-08-19 rapidssl.2018-08-19 digicert_intermediate digicert_root rapidssl",
+//
+//         // 3
+//         "Using certs: bundles equifax.2018-08-19 geotrust.2018-08-19 rapidssl.2018-08-19 digicert_intermediate digicert_root rapidssl",
+//
+//         // 4
+//         "Using certs: bundles equifax.2018-08-19 geotrust.2018-08-19 rapidssl.2018-08-19 digicert_intermediate digicert_root rapidssl",
+//
+//         // fail
+//         "Using certs: bundles equifax.2018-08-19 geotrust.2018-08-19 rapidssl.2018-08-19 digicert_intermediate digicert_root rapidssl",
+//         "Attempting new cert config: node default",
+//         "Using certs: node default", // 5
+//         "Using certs: node default"  // 6
+//       ];
+//       t.deepEqual(cert_log_messages, expected_messages, "expected to use node default certs");
+//       t.pass();
+//       t.end();
+//     },
+//     timeout: function(){
+//       var cert_log_messages =
+//         TestHelper.log.filter(function(entry){return entry.match(/Using certs/i)});
+//       t.fail(JSON.stringify(cert_log_messages) + "\n\n");
+//       t.end();
+//     },
+//     error: function(){
+//       t.fail();
+//       t.end();
+//     },
+//   });
+// });
 
-  realCurrentTime = Math.round(new Date().getTime() / 1000);
-
-  var time = new Date(Date.parse("2018-01-01"));
-  timekeeper.travel(time); // Travel to that date.
-
-  var metricName = "test.metric"+Math.random();
-
-  TestHelper.config.instrumental.tlsVariationTimeout = 500;
-
-  var timeBetweenSends = 2000;
-  var actions = [];
-
-  // node default certs
-  actions.push(function(){
-    TestHelper.config.instrumental.host = "smoke-collector.instrumentalapp.com";
-    sendMetric(metricName, realCurrentTime); // using node default certs
-  });
-  actions.push(function(){
-    sendMetric(metricName, realCurrentTime, {skipInit: true}); // using node default certs
-  });
-
-  // New cert bundle
-  actions.push(function(){
-    TestHelper.config.instrumental.port = 8000;
-    sendMetric(metricName, realCurrentTime); // node default certs fail
-  });
-  actions.push(function(){
-    TestHelper.config.instrumental.port = 8001;
-    sendMetric(metricName, realCurrentTime); // bundle
-  });
-  actions.push(function(){
-    sendMetric(metricName, realCurrentTime, {skipInit: true}); // bundle
-  });
-
-  // Back to node default
-  actions.push(function(){
-    TestHelper.config.instrumental.port = 8000;
-    sendMetric(metricName, realCurrentTime); // bundle fails
-  });
-  actions.push(function(){
-    TestHelper.config.instrumental.port = 8001;
-    sendMetric(metricName, realCurrentTime); // node default
-  });
-  actions.push(function(){
-    sendMetric(metricName, realCurrentTime, {skipInit: true}); // node default
-  });
-
-  actions.forEach(function(action, index){
-    setTimeout(action, index*timeBetweenSends);
-  });
-
-  checkForMetric(metricName, {
-    expectedSum: 6, // failures don't get sent
-    found: function(){
-      var cert_log_messages =
-        TestHelper.log.filter(function(entry){return entry.match(/\bcert/i)});
-      var expected_messages = [
-        "Adding node default ssl cert option",
-        'Found valid cert bundle: equifax.2018-08-19',
-        'Found valid cert bundle: geotrust.2018-08-19',
-        'Found valid cert bundle: rapidssl.2018-08-19',
-        'Found valid cert bundle: digicert_intermediate',
-        'Found valid cert bundle: digicert_root',
-        'Found valid cert bundle: rapidssl',
-        "Attempting new cert config: node default",
-        "Using certs: node default", // 1
-        "Using certs: node default", // 2
-        "Using certs: node default", // fail
-        "Attempting new cert config: bundles equifax.2018-08-19 geotrust.2018-08-19 rapidssl.2018-08-19 digicert_intermediate digicert_root rapidssl",
-
-        // 3
-        "Using certs: bundles equifax.2018-08-19 geotrust.2018-08-19 rapidssl.2018-08-19 digicert_intermediate digicert_root rapidssl",
-
-        // 4
-        "Using certs: bundles equifax.2018-08-19 geotrust.2018-08-19 rapidssl.2018-08-19 digicert_intermediate digicert_root rapidssl",
-
-        // fail
-        "Using certs: bundles equifax.2018-08-19 geotrust.2018-08-19 rapidssl.2018-08-19 digicert_intermediate digicert_root rapidssl",
-        "Attempting new cert config: node default",
-        "Using certs: node default", // 5
-        "Using certs: node default"  // 6
-      ];
-      t.deepEqual(cert_log_messages, expected_messages, "expected to use node default certs");
-      t.pass();
-      t.end();
-    },
-    timeout: function(){
-      var cert_log_messages =
-        TestHelper.log.filter(function(entry){return entry.match(/Using certs/i)});
-      t.fail(JSON.stringify(cert_log_messages) + "\n\n");
-      t.end();
-    },
-    error: function(){
-      t.fail();
-      t.end();
-    },
-  });
-});
-
-test('after cert expiration, fallback to newest cert bundle and stick if node default certs fail', function(t) {
-  setup(t);
-
-  realCurrentTime = Math.round(new Date().getTime() / 1000);
-
-  var time = new Date(Date.parse("2019-01-01"));
-  timekeeper.travel(time); // Travel to that date.
-
-
-  var metricName = "test.metric"+Math.random();
-
-  TestHelper.config.instrumental.tlsVariationTimeout = 500;
-
-  var timeBetweenSends = 2000;
-  var actions = [];
-
-  // node default certs
-  actions.push(function(){
-    TestHelper.config.instrumental.host = "smoke-collector.instrumentalapp.com";
-    sendMetric(metricName, realCurrentTime); // using node default certs
-  });
-  actions.push(function(){
-    sendMetric(metricName, realCurrentTime, {skipInit: true}); // using node default certs
-  });
-
-  // New cert bundle
-  actions.push(function(){
-    TestHelper.config.instrumental.port = 8000;
-    sendMetric(metricName, realCurrentTime); // node default certs fail
-  });
-  actions.push(function(){
-    TestHelper.config.instrumental.port = 8001;
-    sendMetric(metricName, realCurrentTime); // bundle
-  });
-  actions.push(function(){
-    sendMetric(metricName, realCurrentTime, {skipInit: true}); // bundle
-  });
-
-  // Back to node default
-  actions.push(function(){
-    TestHelper.config.instrumental.port = 8000;
-    sendMetric(metricName, realCurrentTime); // bundle fails
-  });
-  actions.push(function(){
-    TestHelper.config.instrumental.port = 8001;
-    sendMetric(metricName, realCurrentTime); // node default
-  });
-  actions.push(function(){
-    sendMetric(metricName, realCurrentTime, {skipInit: true}); // node default
-  });
-
-  actions.forEach(function(action, index){
-    setTimeout(action, index*timeBetweenSends);
-  });
-
-  checkForMetric(metricName, {
-    expectedSum: 6, // failures don't get sent
-    found: function(){
-      var cert_log_messages =
-        TestHelper.log.filter(function(entry){return entry.match(/\bcert/i)});
-      var expected_messages = [
-        "Adding node default ssl cert option",
-        'Ignoring expired cert bundle: equifax.2018-08-19',
-        'Ignoring expired cert bundle: geotrust.2018-08-19',
-        'Ignoring expired cert bundle: rapidssl.2018-08-19',
-        'Ignoring expired cert bundle: equifax.2018-08-19',
-        'Ignoring expired cert bundle: geotrust.2018-08-19',
-        'Ignoring expired cert bundle: rapidssl.2018-08-19',
-        'Found valid cert bundle: digicert_intermediate',
-        'Found valid cert bundle: digicert_root',
-        'Found valid cert bundle: rapidssl',
-        "Attempting new cert config: node default",
-        "Using certs: node default", // 1
-        "Using certs: node default", // 2
-        "Using certs: node default", // fail
-        "Attempting new cert config: bundles digicert_intermediate digicert_root rapidssl",
-
-        // 3
-        "Using certs: bundles digicert_intermediate digicert_root rapidssl",
-
-        // 4
-        "Using certs: bundles digicert_intermediate digicert_root rapidssl",
-
-        // fail
-        "Using certs: bundles digicert_intermediate digicert_root rapidssl",
-        "Attempting new cert config: node default",
-        "Using certs: node default", // 5
-        "Using certs: node default"  // 6
-      ];
-      t.deepEqual(cert_log_messages, expected_messages, "expected to use node default certs");
-      t.pass();
-      t.end();
-    },
-    timeout: function(){
-      var cert_log_messages =
-        TestHelper.log.filter(function(entry){return entry.match(/Using certs/i)});
-      t.fail(JSON.stringify(cert_log_messages) + "\n\n");
-      t.end();
-    },
-    error: function(){
-      t.fail();
-      t.end();
-    },
-  });
-});
+// Skipping this test for now since we rely on system certs. If we add new cert
+// bundles then we should add this back in.
+// test('after cert expiration, fallback to newest cert bundle and stick if node default certs fail', function(t) {
+//   setup(t);
+//
+//   realCurrentTime = Math.round(new Date().getTime() / 1000);
+//
+//   var time = new Date(Date.parse("2019-01-01"));
+//   timekeeper.travel(time); // Travel to that date.
+//
+//
+//   var metricName = "test.metric"+Math.random();
+//
+//   TestHelper.config.instrumental.tlsVariationTimeout = 500;
+//
+//   var timeBetweenSends = 2000;
+//   var actions = [];
+//
+//   // node default certs
+//   actions.push(function(){
+//     TestHelper.config.instrumental.host = "smoke-collector.instrumentalapp.com";
+//     sendMetric(metricName, realCurrentTime); // using node default certs
+//   });
+//   actions.push(function(){
+//     sendMetric(metricName, realCurrentTime, {skipInit: true}); // using node default certs
+//   });
+//
+//   // New cert bundle
+//   actions.push(function(){
+//     TestHelper.config.instrumental.port = 8000;
+//     sendMetric(metricName, realCurrentTime); // node default certs fail
+//   });
+//   actions.push(function(){
+//     TestHelper.config.instrumental.port = 8001;
+//     sendMetric(metricName, realCurrentTime); // bundle
+//   });
+//   actions.push(function(){
+//     sendMetric(metricName, realCurrentTime, {skipInit: true}); // bundle
+//   });
+//
+//   // Back to node default
+//   actions.push(function(){
+//     TestHelper.config.instrumental.port = 8000;
+//     sendMetric(metricName, realCurrentTime); // bundle fails
+//   });
+//   actions.push(function(){
+//     TestHelper.config.instrumental.port = 8001;
+//     sendMetric(metricName, realCurrentTime); // node default
+//   });
+//   actions.push(function(){
+//     sendMetric(metricName, realCurrentTime, {skipInit: true}); // node default
+//   });
+//
+//   actions.forEach(function(action, index){
+//     setTimeout(action, index*timeBetweenSends);
+//   });
+//
+//   checkForMetric(metricName, {
+//     expectedSum: 6, // failures don't get sent
+//     found: function(){
+//       var cert_log_messages =
+//         TestHelper.log.filter(function(entry){return entry.match(/\bcert/i)});
+//       var expected_messages = [
+//         "Adding node default ssl cert option",
+//         'Ignoring expired cert bundle: equifax.2018-08-19',
+//         'Ignoring expired cert bundle: geotrust.2018-08-19',
+//         'Ignoring expired cert bundle: rapidssl.2018-08-19',
+//         'Ignoring expired cert bundle: equifax.2018-08-19',
+//         'Ignoring expired cert bundle: geotrust.2018-08-19',
+//         'Ignoring expired cert bundle: rapidssl.2018-08-19',
+//         'Found valid cert bundle: digicert_intermediate',
+//         'Found valid cert bundle: digicert_root',
+//         'Found valid cert bundle: rapidssl',
+//         "Attempting new cert config: node default",
+//         "Using certs: node default", // 1
+//         "Using certs: node default", // 2
+//         "Using certs: node default", // fail
+//         "Attempting new cert config: bundles digicert_intermediate digicert_root rapidssl",
+//
+//         // 3
+//         "Using certs: bundles digicert_intermediate digicert_root rapidssl",
+//
+//         // 4
+//         "Using certs: bundles digicert_intermediate digicert_root rapidssl",
+//
+//         // fail
+//         "Using certs: bundles digicert_intermediate digicert_root rapidssl",
+//         "Attempting new cert config: node default",
+//         "Using certs: node default", // 5
+//         "Using certs: node default"  // 6
+//       ];
+//       t.deepEqual(cert_log_messages, expected_messages, "expected to use node default certs");
+//       t.pass();
+//       t.end();
+//     },
+//     timeout: function(){
+//       var cert_log_messages =
+//         TestHelper.log.filter(function(entry){return entry.match(/Using certs/i)});
+//       t.fail(JSON.stringify(cert_log_messages) + "\n\n");
+//       t.end();
+//     },
+//     error: function(){
+//       t.fail();
+//       t.end();
+//     },
+//   });
+// });
 
 test('old agent works with new elb', function(t) {
   setup(t);
